85594f6998fa74f1174587447052391c82f05383
Thomas Haller <Thomas.Haller@lab10.coop>
2020-01-23 19:32:29 +0100

    debug fix "OutOfRange"

--- DynamicWorldSandbox.Unity/DynamicWorldSandbox/Assets/Scripts/UIDebugInfo/DebugCurrentWaterTile.cs
+++ DynamicWorldSandbox.Unity/DynamicWorldSandbox/Assets/Scripts/UIDebugInfo/DebugCurrentWaterTile.cs
@@ -11,7 +11,16 @@ public class DebugCurrentWaterTile : TextUpdateMonoBehaviour
     {
         int playerPositionX = Convert.ToInt32(Camera.main.transform.position.x);
         int playerPositionY = Convert.ToInt32(Camera.main.transform.position.y);
-        textComponent.text = DynamicWorldSandbox.Model.Modules.HydrationModule.HydrationModule.LastInitializedInstance.HydrationValues[playerPositionX, playerPositionY].ToString("#.###");
+        if (playerPositionX >= 0 && playerPositionX < DynamicWorldSandbox.Model.Modules.HydrationModule.HydrationModule.LastInitializedInstance.World.Width &&
+           playerPositionY >= 0 && playerPositionY <  DynamicWorldSandbox.Model.Modules.HydrationModule.HydrationModule.LastInitializedInstance.World.Height) 
+        {
+            textComponent.text = DynamicWorldSandbox.Model.Modules.HydrationModule.HydrationModule.LastInitializedInstance.HydrationValues[playerPositionX, playerPositionY].ToString("#.###");            
+        }
+        else
+        {
+            textComponent.text = "OutOfWorld.";
+        }
+        
 
     }
 }
